d1533f347f1963677b9dc169256b437ad2291c99
Shahzad-Ali-Official <shahzadofficialfirms@gmail.comgit config user.namegit config --global user.email shahzadofficialfirms@gmail.com>
2025-10-21 00:15:54 +0500

    ok5

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: 'index.html', dest: '' }
-      ]
-    })
-  ],
+  plugins: [react()],
 })
